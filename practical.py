88552e8be348f4d85e7cbe91b05f812a14e7f8ca
Yolisa Magingxa <yolisamagingxa6@gmail.com>
2024-04-28 23:35:31 +0200

    Commiting for last time the practical file

--- practical.py
+++ practical.py
@@ -33,4 +33,30 @@ plt.xlabel("wins")
 plt.ylabel("Losses")
 plt.show()
 
+#Histogram, visualize how many goals scored per match in 2020
+goals_per_match = df['Goals per match']
+size = 365
 
+plt.hist(goals_per_match)
+plt.tittle("Histogram that shows the goaling performance of the team in 2020")
+plt.xlabel("Goals per match")
+plt.ylabel("Frequency")
+
+#Bar chart that compares the goals and the own goals 
+own_goals = df["Own goals"]
+self_scored_goals = df["Goals"]
+
+plt.figure(figsize=(12,6))
+plt.bar(own_goals,self_scored_goals)
+plt.xlabel("Own goals")
+plt.ylabel("Self Scored goals")
+plt.show()
+
+#Pie chart showiing different ways of how the  goals were scored for the first match of Arsenal
+goals= ['Own goals','Goals with right foot', 'Goals with left foot','pernalty scored']
+scores = [1,4,3,0]
+
+plt.figure(figsize=(7,7)
+plt.pie(scores,label=goals,autput = '%1.1f%%',startangle=140)
+plt.tittle('Different types of goals scored')
+plt.show()
